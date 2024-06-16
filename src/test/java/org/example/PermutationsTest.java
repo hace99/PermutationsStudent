efbca07a03ca068f4675f9495053c491cf48504c
hace99 <lorenzo.ferrini99@gmail.com>
2024-06-16 11:33:07 +0200

    2

--- src/test/java/org/example/PermutationsTest.java
+++ src/test/java/org/example/PermutationsTest.java
@@ -9,27 +9,6 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class PermutationsTest {
     @Test
-    public void threeDifferentValues2() {
-        Permutations permutations = new Permutations();
-        int[] nums = {1, 2, 3};
-        List<List<Integer>> result = permutations.permute(nums);
-
-        assertEquals(6, result.size());
-
-        List<List<Integer>> expectedPermutations = Arrays.asList(
-                Arrays.asList(1, 2, 3),
-                Arrays.asList(1, 3, 2),
-                Arrays.asList(2, 3, 1),
-                Arrays.asList(2, 1, 3),
-                Arrays.asList(3, 1, 2),
-                Arrays.asList(3, 2, 1)
-        );
-
-        for (List<Integer> expected : expectedPermutations) {
-            assertTrue(result.contains(expected));
-        }
-    }
-    @Test
     public void threeDifferentValues() {
         Permutations permutations = new Permutations();
         int[] nums = {1, 2, 3};
